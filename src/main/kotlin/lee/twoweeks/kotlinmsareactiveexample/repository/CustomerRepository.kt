c774124c985dd417f4dc092b285014e88dcee640
Joohan Lee <joohan33@gmail.com>
2020-07-02 00:15:49 +0900

    Use reactive mongo repository

--- src/main/kotlin/lee/twoweeks/kotlinmsareactiveexample/repository/CustomerRepository.kt
+++ src/main/kotlin/lee/twoweeks/kotlinmsareactiveexample/repository/CustomerRepository.kt
@@ -2,18 +2,8 @@ package lee.twoweeks.kotlinmsareactiveexample.repository
 
 import lee.twoweeks.kotlinmsareactiveexample.model.Customer
 import org.bson.types.ObjectId
-import org.springframework.data.mongodb.core.ReactiveMongoTemplate
-import org.springframework.data.mongodb.core.find
-import org.springframework.data.mongodb.core.findById
-import org.springframework.data.mongodb.core.query.Criteria.where
-import org.springframework.data.mongodb.core.query.Query
-import org.springframework.data.mongodb.core.query.isEqualTo
-import org.springframework.data.mongodb.core.remove
-import org.springframework.data.repository.reactive.ReactiveCrudRepository
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository
 import org.springframework.stereotype.Repository
-import reactor.core.publisher.Mono
-import reactor.kotlin.core.publisher.toMono
-import javax.annotation.PostConstruct
 
 /**
  * Created by Joohan Lee on 2020/04/21
@@ -23,7 +13,7 @@ import javax.annotation.PostConstruct
 //interface CustomerRepository : ReactiveCrudRepository<Customer, Int>
 
 @Repository
-interface CustomerRepository : ReactiveCrudRepository<Customer, ObjectId>, CustomerRepositoryCustom{
+interface CustomerRepository : ReactiveMongoRepository<Customer, ObjectId>, CustomerRepositoryCustom{
 //    fun create(customer: Mono<Customer>) = template.save(customer)
 //    fun findById(id : Int) = template.findById<Customer>(id)
 //    fun deleteById(id: Int) = template.remove(<Customer>(Query(where("_id").isEqualTo(id)))
